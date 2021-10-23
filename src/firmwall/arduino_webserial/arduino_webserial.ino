83ee74580b3a1753498bbc989547c0dd7f7c433e
estea8968 <estea8968@gmail.com>
2021-10-23 09:27:14 +0800

    update urltxt json web_serial_arduino

--- src/firmwall/arduino_webserial/arduino_webserial.ino
+++ src/firmwall/arduino_webserial/arduino_webserial.ino
@@ -1,24 +1,9 @@
-
+/*
+ * 更新日期110/10/22 estea chen
+ */
 #include <Servo.h>
-#include "DHTStable.h"
+#include <DHTStable.h>
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-//oled
-#include <string.h>
-#include <Arduino.h>
-#include <U8g2lib.h>
-
-//#ifdef U8X8_HAVE_HW_SPI
-//#include <SPI.h>
-//#endif
-//#ifdef U8X8_HAVE_HW_I2C
-//#include <Wire.h>
-//#endif
-
-U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-//oled end
-
-LiquidCrystal_I2C lcd(0x27, 16, 2);  //設定LCD
 
 DHTStable DHT;
 
@@ -29,7 +14,7 @@ char* serialString()
   //static char str[21]; // For strings of max length=20
   static char str[21]; // For strings of max length=20
   if (!Serial.available()) return NULL;
-  delay(32); // wait for all characters to arrive
+  delay(16); // wait for all characters to arrive
   memset(str,0,sizeof(str)); // clear str
   byte count=0;
   while (Serial.available())
@@ -49,14 +34,7 @@ char* serialString()
 
 void setup() {
   Serial.begin(115200);
-  //LCD
-  lcd.init(); //初始化LCD 
-  lcd.begin(16, 2); //初始化 LCD，代表我們使用的LCD一行有16個字元，共2行。
-  lcd.backlight(); //開啟背光
-  //oled
-  u8g2.begin();
-  u8g2.enableUTF8Print();  //啟用UTF8文字的功能  
-  
+   
 }
 
 void loop() 
@@ -82,16 +60,8 @@ void loop()
     char* inputValue = strtok(NULL, "#");
     //取出第4個值
     int inputTime = atoi(strtok(NULL, "#"));
-    //setPinmode
-    if(strcmp(commandString, "pinMode")== 0){
-      int digitalPin = atoi(inputPin);
-      if(strcmp(inputValue, "output")== 0){
-        pinMode(digitalPin, OUTPUT);        
-      }else{
-        pinMode(digitalPin, INPUT);
-      }
-      //Serial.println(inputValue);
-    }
+
+       
     //超音波
     if(strcmp(commandString, "HC-SR04")== 0){
        long duration, cm; 
@@ -111,41 +81,7 @@ void loop()
        cm = (duration/2) / 29.1;         // 將時間換算成距離 cm
        Serial.println(cm);        
     }
-    //oled 16x2
-    //format: l#string#row
-
-    if(strcmp(commandString, "o") == 0) {
-        u8g2.setFont(u8g2_font_unifont_t_chinese1); //使用字型
-        u8g2.firstPage();
-        do {
-          if(atoi(inputValue) == 0){
-            u8g2.setCursor(0, 14);
-          }
-          if(atoi(inputValue) == 1 ){
-            u8g2.setCursor(0, 35);
-          }
-          if(atoi(inputValue) == 2 ){
-            u8g2.setCursor(0, 60);
-          }
-          u8g2.print(inputPin);
-        }while ( u8g2.nextPage() );
-            //delay(1000);
-    }
     
-    //lcd 16x2
-    //format: l#string#row
-    if(strcmp(commandString, "l") == 0) {
-      if(strcmp(inputPin, "clear") == 0) {
-          lcd.clear();
-      }else{
-          if(atoi(inputValue) == 0){
-            lcd.setCursor(0,0);
-          }else{
-            lcd.setCursor(0,1);
-          }
-          lcd.print(inputPin);  
-      }
-    }
     //dht11
     if(strcmp(commandString, "dht11Read") == 0){
       int chk = DHT.read11(atoi(inputPin));
@@ -175,8 +111,9 @@ void loop()
     }
     //數位讀取
     if(strcmp(commandString, "digitalRead") == 0){
-      int digitalPin = atoi(inputPin);
-      Serial.println(digitalRead(digitalPin));
+      pinMode(atoi(inputPin),OUTPUT);
+      Serial.println(digitalRead(atoi(inputPin)));
+      
     }
     //類比寫入
     if(strcmp(commandString, "analogWrite") == 0){
@@ -184,10 +121,11 @@ void loop()
     }
     //數位寫入
     if(strcmp(commandString, "digitalWrite") == 0){
-      int digitalPin = atoi(inputPin);
-      digitalWrite(digitalPin,atoi(inputValue));
-    }
+         pinMode(atoi(inputPin),OUTPUT);
+         digitalWrite(atoi(inputPin),atoi(inputValue));
+     }
     needPrompt=true;
     //delay(1000);
   }
+  
 }
