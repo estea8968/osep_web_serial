2d90bf96a45521719c2b8a4b36a21c59053b628d
estea8968 <estea8968@gmail.com>
2022-01-18 07:49:34 +0800

    更新esp32積木

--- src/firmware/esp32_webserial/esp32_webserial.ino
+++ src/firmware/esp32_webserial/esp32_webserial.ino
@@ -1,5 +1,5 @@
 /*
- * 111/01/17
+ * 111/01/18
  */
 //#include<WiFi.h>
 //#include <Servo.h>
@@ -142,6 +142,80 @@ void loop() {
       char* inputTime =strtok(NULL, "#");
       //Serial.println(inputTime);
 
+      if(strcmp(commandString, "sh") == 0){
+      int r = 0;
+      int g = 0;
+      int b = 0;
+      char * led_value[]={"","","","","","","","","","","","","","","","","","","","","","","",""};
+      char * bb ="";
+      int i = 0;
+      int sp;
+      bb = strtok(inputValue, ",");
+      led_value[i] = bb;
+      //Serial.println(led_value[i]);
+      i++; 
+      while( bb!= NULL ){
+        bb = strtok(NULL, ",");
+        led_value[i] = bb;
+        //Serial.println(led_value[i]);
+        i++;        
+      }
+      Adafruit_NeoPixel pixels(NUMPIXELS, atoi(inputPin), NEO_GRB + NEO_KHZ800);
+      pixels.begin();
+      for ( i=0;i<24;i++){
+        if (led_value[i] > 0){
+            sp = atoi(led_value[i])-1;
+          i++;
+          if( atoi(led_value[i]) == 0) {
+            i++;
+            r = atoi(led_value[i]);
+            g = 0;
+            b = 0;
+          }else if( atoi(led_value[i]) == 1){
+            i++;
+            r = atoi(led_value[i])*3;
+            g = atoi(led_value[i]);
+            b = 0;
+          }else if( atoi(led_value[i]) == 2){
+            i++;
+            r = atoi(led_value[i]);
+            g = atoi(led_value[i]);
+            b = 0;
+          }else if( atoi(led_value[i]) == 3){
+            i++;
+            r = 0;
+            g = atoi(led_value[i]);
+            b = 0;
+          }else if( atoi(led_value[i]) == 4){
+            i++;
+            r = 0;
+            g = 0;
+            b = atoi(led_value[i]);
+          }else if( atoi(led_value[i]) == 5){
+            i++;
+            r = 0;
+            g = atoi(led_value[i]);
+            b = atoi(led_value[i]);
+          }else if( atoi(led_value[i]) == 6){
+            i++;
+            r = atoi(led_value[i]);
+            g = 0;
+            b = atoi(led_value[i]);
+          }else if( atoi(led_value[i]) == 7){
+            i++;
+            r = atoi(led_value[i]);
+            g = atoi(led_value[i]);
+            b = atoi(led_value[i]);
+          }
+          pixels.setPixelColor(sp, pixels.Color(r, g, b));
+        }else{
+          i++;
+          i++;
+        }
+      }
+      pixels.show(); 
+    }
+    
       //ws2812
       if(strcmp(commandString, "ws") == 0){
         int r = atoi(strtok(inputValue,","));
