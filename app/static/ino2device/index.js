854b1bd5c4754f20b9bff31390678450730bbc3a
estea8968 <estea8968@gmail.com>
2022-03-19 05:43:09 +0800

    更新scratch主程式

--- app/static/ino2device/index.js
+++
@@ -1,8 +0,0 @@
-let arduinoCli = require('electron-arduino-cli');
-/*
-const cli = arduinoCli('/usr/local/bin/arduino-cli', {
-  directories: {
-    user: '~/arduino-cli/sketches',
-    data: '~/arduino-cli/data',
-  },
-});*/
