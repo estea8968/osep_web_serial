1224da2297be2b6393005d0eeae552fb35606bfa
estea8968 <estea8968@gmail.com>
2022-03-18 21:47:24 +0800

    更新microbit

---
+++ app/static/ino2device/index.js
@@ -0,0 +1,8 @@
+let arduinoCli = require('electron-arduino-cli');
+/*
+const cli = arduinoCli('/usr/local/bin/arduino-cli', {
+  directories: {
+    user: '~/arduino-cli/sketches',
+    data: '~/arduino-cli/data',
+  },
+});*/
