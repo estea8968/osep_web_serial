abfc57f9e5d83c371eae23edb956d5e763d0900e
estea8968 <estea8968@gmail.com>
2023-03-31 07:53:45 +0800

    增加linkit7697韌體

---
+++ src/firmware/linkit7697/linkit7697_webserial.ino
@@ -0,0 +1,220 @@
+/*
+ * 更新日期112/03/30 estea chen
+ * 
+ */
+#include <Servo.h>
+//版本號
+char* version="1120330";
+Servo myservo;  // create servo object to control a servo
+
+char* serialString()
+{
+  //static char str[21]; // For strings of max length=20
+  static char str[64]; // For strings of max length=20
+  if (!Serial.available()) return NULL;
+  delay(6); // wait for all characters to arrive
+  memset(str,0,sizeof(str)); // clear str
+  byte count=0;
+  while (Serial.available())
+  {
+    char c=Serial.read();
+    //if (c>=32 && count<sizeof(str)-1)
+    //c最大35
+    //if (c>=32 && count<sizeof(str)-1)
+    //{
+      str[count]=c;
+      count++;
+    //}
+  }
+  str[count]='\0'; // make it a zero terminated string
+  return str;
+}
+
+void setup() {
+  Serial.begin(115200);
+}
+
+
+
+void loop() 
+{
+  static boolean needPrompt=true;
+  char* inputData;
+  if (needPrompt)
+  {
+    //Serial.print("Please enter inputs and press enter at the end:\n");
+    needPrompt=false;
+  }
+  inputData= serialString();
+
+  if (inputData!=NULL)
+  {
+    //取出命令、腳位、值、時間
+    char* commandString = strtok(inputData, "#"); 
+    char* inputPin = strtok(NULL, "#");
+    //取出第3個值
+    char* inputValue = strtok(NULL, "#");
+    //取出第4個值
+    char* inputTime = strtok(NULL, "#");
+
+    //版本
+    if(strcmp(commandString, "ver") == 0){
+      Serial.println(version);
+    }
+      
+    
+    //tone
+    if(strcmp(commandString, "tonePlay") == 0){
+      int tonePin = atoi(inputPin);
+      int delayTime = atoi(inputTime)-3;
+      tone(tonePin, atoi(inputValue),delayTime);
+      delay(delayTime);
+      noTone(atoi(inputPin));
+      delay(2);
+    }
+    //伺服馬達
+    if(strcmp(commandString, "servoWrite") == 0){
+      myservo.attach(atoi(inputPin));
+      myservo.write(atoi(inputValue));
+      delay(100);
+    }
+    //類比讀取
+    if(strcmp(commandString, "analogRead") == 0){
+      Serial.print("A");
+      Serial.print(atoi(inputPin));
+      Serial.print(":");
+      Serial.println(analogRead(atoi(inputPin)));
+    }
+    //數位讀取
+    if(strcmp(commandString, "digitalRead") == 0){
+      pinMode(atoi(inputPin),INPUT);
+      //2-19
+      Serial.print("D");
+      Serial.print(atoi(inputPin));
+      Serial.print(":");
+      Serial.println(digitalRead(atoi(inputPin)));
+    }
+        
+    //類比寫入
+    if(strcmp(commandString, "analogWrite") == 0){
+      analogWrite(atoi(inputPin),atoi(inputValue));
+    }
+    //數位寫入
+    if(strcmp(commandString, "digitalWrite") == 0){
+         pinMode(atoi(inputPin),OUTPUT);
+         digitalWrite(atoi(inputPin),atoi(inputValue));
+     }
+    needPrompt=true;
+    //delay(1000);
+  }
+  
+}
+
+
+/*void getG5(unsigned char ucData)//取G5的值
+{
+  static unsigned int ucRxBuffer[250];
+  static unsigned int ucRxCnt = 0;
+  ucRxBuffer[ucRxCnt++] = ucData;
+  if (ucRxBuffer[0] != 0x42 && ucRxBuffer[1] != 0x4D)//数据头判断
+  {
+    ucRxCnt = 0;
+    return;
+  }
+
+  if (ucRxCnt > 38)//数据位判断//G5S为32，G5ST为38
+
+  {
+       pm_cf_10=(int)ucRxBuffer[4] * 256 + (int)ucRxBuffer[5];      //大气环境下PM2.5浓度计算        
+       pm_cf_25=(int)ucRxBuffer[6] * 256 + (int)ucRxBuffer[7];
+       pm_cf_100=(int)ucRxBuffer[8] * 256 + (int)ucRxBuffer[9];
+       pm_at_10=(int)ucRxBuffer[10] * 256 + (int)ucRxBuffer[11];               
+       pm_at_25=(int)ucRxBuffer[12] * 256 + (int)ucRxBuffer[13];
+       pm_at_100=(int)ucRxBuffer[14] * 256 + (int)ucRxBuffer[15];
+       particulate03=(int)ucRxBuffer[16] * 256 + (int)ucRxBuffer[17];
+       particulate05=(int)ucRxBuffer[18] * 256 + (int)ucRxBuffer[19];
+       particulate10=(int)ucRxBuffer[20] * 256 + (int)ucRxBuffer[21];
+       particulate25=(int)ucRxBuffer[22] * 256 + (int)ucRxBuffer[23];
+       Temperature = ((int)ucRxBuffer[24] * 256 + (int)ucRxBuffer[25])/10;
+       Humidity = ((int)ucRxBuffer[26] * 256 + (int)ucRxBuffer[27])/10;
+    if (pm_cf_25 >  999)//如果PM2.5数值>1000，返回重新计算
+    {
+      ucRxCnt = 0;
+      return;
+    }
+    ucRxCnt = 0;
+    return;
+  }
+}*/
+
+/*uint64_t stringToUint_64(String value) {
+  int stringLenght = value.length();
+
+  uint64_t uint64Value = 0x0;
+  for(int i = 0; i<=stringLenght-1; i++) {
+    char charValue = value.charAt(i);
+    uint64Value = 0x10 * uint64Value;
+    uint64Value += stringToHexInt(charValue);
+  }
+
+  return uint64Value;
+}*/
+
+/*int stringToHexInt(char value) {
+  switch(value) {
+    case '0':
+      return 0;
+      break;
+    case '1':
+      return 0x1;
+      break;
+    case '2':
+      return 0x2;
+      break;
+    case '3':
+      return 0x3;
+      break;
+    case '4':
+      return 0x4;
+      break;
+    case '5':
+      return 0x5;
+      break;
+    case '6':
+      return 0x6;
+      break;
+    case '7':
+      return 0x7;
+      break;
+    case '8':
+      return 0x8;
+      break;
+    case '9':
+      return 0x9;
+      break;
+    case 'A':
+    case 'a':
+      return 0xA;
+      break;
+    case 'B':
+    case 'b':
+      return 0xB;
+      break;
+    case 'C':
+    case 'c':
+      return 0xC;
+      break;
+    case 'D':
+    case 'd':
+      return 0xD;
+      break;
+    case 'E':
+    case 'e':
+      return 0xE;
+      break;
+    case 'F':
+    case 'f':
+      return 0xF;
+      break;
+  }
+}*/
