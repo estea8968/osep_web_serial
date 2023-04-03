2bc6cdfc605b1f727829b327195d74544f4dd374
estea8968 <estea8968@gmail.com>
2023-04-03 14:13:12 +0800

    更新linkit7697 燒錄批次檔

--- src/firmware/linkit7697/burn/build.bat
+++ src/firmware/linkit7697/burn/build.bat
@@ -2,5 +2,5 @@
 set one=%1
 set two=%2 
 if "%one%"=="" (
-echo 請輸入連線com port) else ( if "%two%"=="" (echo 請輸入韌體檔案名稱) else echo upload.exe -c COM24 -f linkit7697.bin -t cm4 -p mt7697 ) 
+echo 請輸入連線com port) else ( if "%two%"=="" (echo 請輸入韌體檔案名稱) else echo upload.exe -c %1 -f %2 -t cm4 -p mt7697 ) 
 
